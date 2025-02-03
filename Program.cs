907895d32267cdff6ad9054a86d2b4e12227e7b8
SBC <seobc789@gmail.com>
2025-02-03 17:50:30 +0900

    함수 형태 연습
    곱셈 함수 형태 연습
    다른 인자를 갖는 함수의 오버로딩
    
    컴파일 과정에서 자동 mangling

--- Program.cs
+++ Program.cs
@@ -9,6 +9,9 @@ namespace PJT2502031
         static int[,] data = new int[size, size];
 
         // 함수의 형태
+        /// <summary>
+        /// 배열 초기화
+        /// </summary>
         static void Initialize() // PascalCase
         {
             int num = 1;
@@ -23,6 +26,9 @@ namespace PJT2502031
             }
         }
 
+        /// <summary>
+        /// 배열 출력
+        /// </summary>
         static void Print()
         {
             for (int j = 0; j < size; j++)
@@ -35,15 +41,8 @@ namespace PJT2502031
             }
         }
 
-        static void Main(string[] args)
+        static void PrintStar()
         {
-            // 배열 초기화
-            Initialize();
-
-            // 배열 출력
-            Print();
-
-            // 별 찍기
             for (int j = 1; j <= size; j++)
             {
                 for (int k = 1; k <= size - j; k++)
@@ -56,6 +55,36 @@ namespace PJT2502031
                 }
                 Console.WriteLine();
             }
+        }
+
+        /// <summary>
+        /// 두 정수 곱셈
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        static int Multiply(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+        static float Multiply(float num1, float num2)
+        {
+            return num1 * num2;
+        }
+
+
+        static void Main(string[] args)
+        {
+            // 배열 초기화
+            //Initialize();
+
+            // 배열 출력
+            //Print();
+
+            // 별 찍기
+            //PrintStar();
+
+            // 곱셈 테스트
+            Console.WriteLine(Multiply(5, 7.3f));
         }
     }
 }

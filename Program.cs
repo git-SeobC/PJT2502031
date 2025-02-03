a81fae8154a20cbafc39793a75330ef038a755e2
SBC <seobc789@gmail.com>
2025-02-03 15:13:57 +0900

    2차원 배열 초기화
    2차원 배열 초기화 및 값 출력

--- Program.cs
+++ Program.cs
@@ -5,6 +5,29 @@
         static void Main(string[] args)
         {
             int size = 10;
+            int[,] data = new int[size, size];
+            int num = 1;
+
+            // 배열 초기화
+            for (int j = 0; j < size; j++)
+            {
+                for (int i = 0; i < size; i++)
+                {
+                    //data[j, i] = (j * size) + (i + 1);
+                    data[j, i] = num++;
+                }
+            }
+
+            // 배열 출력
+            for (int j = 0; j < size; j++)
+            {
+                for (int i = 0; i < size; i++)
+                {
+                    Console.Write(data[j, i].ToString() + "\t");
+                }
+                Console.WriteLine();
+            }
+
             // 별 찍기
             for (int j = 1; j <= size; j++)
             {
